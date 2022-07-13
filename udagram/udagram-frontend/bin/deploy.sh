0d7e4caa8edf4a075efd5d0b8548f08cab3301e9
fady hany <104906845+Fadyhany10@users.noreply.github.com>
2022-07-13 16:22:44 +0200

    Update deploy.sh

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./www s3://project-02/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://project-02/
+aws s3 cp --recursive --acl public-read ./www s3://project-02/ --region us-east-1
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://project-02/ --region us-east-1
